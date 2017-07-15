ef32113e216950f674cba1d71802bf32f695e135
Max Ma <mashuoyi111@sina.com>
2017-07-14 17:45:14 -0700

    initial commit
    empty Android App

---
+++ app/src/main/java/com/example/administrator/my_weather_report/Welcome_Page.java
@@ -0,0 +1,13 @@
+package com.example.administrator.my_weather_report;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome__page);
+    }
+}
