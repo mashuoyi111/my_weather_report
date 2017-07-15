e219424e093355adbd46e5898d7321712cbef5e4
Max Ma <mashuoyi111@sina.com>
2017-07-14 17:52:07 -0700

    test commit

--- app/src/main/java/com/example/administrator/my_weather_report/Welcome_Page.java
+++ app/src/main/java/com/example/administrator/my_weather_report/Welcome_Page.java
@@ -9,5 +9,6 @@ public class Welcome_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome__page);
+        //hello
     }
 }
